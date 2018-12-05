455e64969a094eeff79d54186bfbb62cc9a998c9
Julian Schmuckli <julian@schmuckli.net>
2018-12-05 20:44:57 +0100

    Added Task 2 for Day 3

---
+++ Day 3/task2.py
@@ -0,0 +1,52 @@
+input = open("input.txt", "r").read()
+tasks = input.split("\n")
+
+# Creates a list containing 100000 lists, each of 100000 items, all set to 0
+print "Generating map..."
+w, h = 1000, 1000;
+matrix = [[0 for x in range(w)] for y in range(h)]
+
+count_inches = 0
+
+for task in tasks:
+    if task != "":
+        parts = task.split(" ")
+        number = parts[0].replace("#","")
+        coordinates = parts[2].replace(":","").split(",");
+        size = parts[3].split("x");
+
+        for x in range(int(coordinates[0]), int(coordinates[0]) + int(size[0])):
+            for y in range(int(coordinates[1]), int(coordinates[1]) + int(size[1])):
+                print "Placing: ", x, "," , y
+                if matrix[x][y] == 0:
+                    matrix[x][y] = number
+                else:
+                    if matrix[x][y] != "X":
+                        matrix[x][y] = "X"
+                        count_inches += 1
+                        print "Found double inch: ", x, ",", y
+                    else:
+                        print "Duplicate inch: ", x, ",", y
+
+# Check which ID does not overlap
+id = 0;
+for task in tasks:
+    if task != "":
+        parts = task.split(" ")
+        number = parts[0].replace("#","")
+        coordinates = parts[2].replace(":","").split(",");
+        size = parts[3].split("x");
+
+        overlaps = False;
+
+        for x in range(int(coordinates[0]), int(coordinates[0]) + int(size[0])):
+            if overlaps == False:
+                for y in range(int(coordinates[1]), int(coordinates[1]) + int(size[1])):
+                    print "Searching: ", x, "," , y
+                    if matrix[x][y] != number:
+                        overlaps = True
+        if overlaps == False:
+            id = number
+            break
+
+print "ID which does not overlap: ", id
